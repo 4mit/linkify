298ebb097d7c18ec6bd2341ec6a294f5c4cb6f3c
Amit Kumar Soni <amitsoniforbhu@gmail.com>
2022-03-13 14:10:39 +0530

    v4
    v4

--- pages/components/nav/index.js
+++ pages/components/nav/index.js
@@ -1,5 +1,39 @@
 import React from "react";
-import NavConfig from "./config";
+
+import DashboardIcon from "./icons/dashboard.svg";
+import CalanderIcon from "./icons/calander.svg";
+import ProjectsIcon from "./icons/projects.svg";
+import TasksIcon from "./icons/tasks.svg";
+//import { ReactComponent as ReportIcon } from "./icons/report.svg";
+import NavigateIcon from "./icons/navigate.svg";
+import QuestionIcon from "./icons/question.svg";
+
+const navConfig = [
+  {
+    title: "web site link",
+    icon: <DashboardIcon />,
+  },
+  {
+    title: "Projects",
+    icon: <ProjectsIcon />,
+  },
+  {
+    title: "My Tasks",
+    icon: <TasksIcon />,
+  },
+  {
+    title: "Calander",
+    icon: <CalanderIcon />,
+  },
+  {
+    title: "Time Manage",
+    icon: <NavigateIcon />,
+  },
+  {
+    title: "Setting",
+    icon: <QuestionIcon />,
+  },
+];
 
 const Nav = () => {
   return (

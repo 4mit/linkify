444842ea8b9ee76c3592c3e9050e2ebca7f98496
Amit Kumar Soni <amitsoniforbhu@gmail.com>
2022-03-13 14:11:58 +0530

    v6
    v6

--- pages/components/links/index.js
+++ pages/components/links/index.js
@@ -1,5 +1,25 @@
-import { links } from "./config";
-
+const links = [
+  {
+    title: "poshan tracker",
+    link: "https://www.poshantracker.in/admin/",
+    supprts: ["ie", "chrome"],
+  },
+  {
+    title: "dbt",
+    link: "https://wcd-dbtws.nic.in/portal/login",
+    supprts: ["ie", "chrome"],
+  },
+  {
+    title: "SHEBOX",
+    link: "http://www.shebox.nic.in/organization/stateAdminLogin",
+    supprts: ["ie", "chrome"],
+  },
+  {
+    title: "youtube",
+    link: "https://www.youtube.com/",
+    supprts: ["ie", "chrome"],
+  },
+];
 const VibhagLink = () => {
   return (
     <div>
